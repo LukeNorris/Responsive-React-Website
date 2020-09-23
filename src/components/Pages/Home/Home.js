92b4aca9c92d0785406ed0a78ad0c1754ce387b1
Luke Norris <luke.mjn@gmail.com>
2020-09-23 22:14:22 +0200

    added footer and styling

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import HeroSection from '../../HeroSection/HeroSection'
 import Cards from '../../Cards/Cards'
 import '../../../App.css'
+import Footer from '../../Footer/Footer';
 
 function Home() {
     return (
         <>
             <HeroSection />
-            <Cards />    
+            <Cards /> 
+            <Footer />   
         </>
     )
 }
